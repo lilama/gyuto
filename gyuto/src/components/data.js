8c457f17debe3d0cd7cd8482a0413fd2f110e1c5
Pedro <seromenho.pedro@gmail.com>
2018-10-22 17:07:24 +0200

    components setup

---
+++ gyuto/src/components/data.js
@@ -0,0 +1,86 @@
+export const video = [
+    {
+        id: "",
+        title: [{
+            fr: "",
+            en: "",
+        }],
+        type: "",
+        quote_fr: "",
+        quote_en: "",
+        legend_fr: "",
+        legend_en: "",
+        url: "",
+        gif: "",
+        duration: ""
+    }
+]
+
+export const event = [
+    {
+        id: "",
+        title: [{
+            fr: "",
+            en: "",
+        }],
+        date: "",
+        hour: "",
+        place: "",
+        description_fr: "",
+        description_en: "",
+        url: "",
+    }
+]
+
+export const images = [
+    {
+        id: "",
+        title: [{
+            fr: "",
+            en: "",
+        }],
+        date: "",
+        cover: "",
+        images: [{
+            description_fr: "",
+            description_en: "",
+            images: [""]
+
+        }],
+    }
+]
+
+export const musique = [
+    {
+        id: "",
+        video_url: "",
+        title: [{
+            fr: "",
+            en: "",
+        }],
+        description: [{
+            images: "",
+            text: [{
+                fr: "",
+                en: "",
+            }],
+        }],
+        initials: "B",
+        area: "Brussels",
+        skills: "Human Ressources",
+        type: "coaching",
+        duration: "8h"
+    }
+]
+
+export const info = [
+    {
+        date: "01-09-2018",
+        name: "Becode",
+        initials: "B",
+        area: "Brussels",
+        skills: "Human Ressources",
+        type: "coaching",
+        duration: "8h"
+    }
+]
