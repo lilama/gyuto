567a7c49a7d3db995f6e38517f59a92a9335cd63
baptistefkt <baptistefirket@gmail.com>
2018-10-23 13:56:20 +0200

    addbulma, fix partials paths, remove not needed index.css and what we don't need in app.js

---
+++ gyuto-web/src/components/pageEvents/PageEvents.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class PageEvents extends Component {
+  render() {
+    return (
+      <div className="columns">
+        <div className="column">1</div>
+        <div className="column">2</div>
+        <div className="column">3</div>
+      </div>
+    );
+  }
+}
+
+export default PageEvents;
