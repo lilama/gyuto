cbfb895091b6d1c6c2cdec8707dc84f7ae268029
baptistefkt <baptistefirket@gmail.com>
2018-10-23 16:17:13 +0200

    update app.js

--- gyuto-web/src/App.js
+++ gyuto-web/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import PageEvents from "./components/pageEvents/PageEvents";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello world</h1>
+        <PageEvents />
       </div>
     );
   }
