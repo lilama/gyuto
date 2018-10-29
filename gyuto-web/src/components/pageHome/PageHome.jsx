73aaebc6fb18982cf6cc6b8297c5c8d50d593ffd
Pedro <seromenho.pedro@gmail.com>
2018-10-29 16:41:21 +0100

    update jsx

---
+++ gyuto-web/src/components/pageHome/PageHome.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Mandala from './assets/mandala/Mandala'
+
+class PageHome extends Component {
+    state = {}
+    render() {
+        return (
+            <Mandala></Mandala>
+        );
+    }
+}
+
+export default PageHome;
