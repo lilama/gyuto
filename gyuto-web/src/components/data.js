1ccae83d99c7aed577bc361ce4314fddc7329514
Pedro <seromenho.pedro@gmail.com>
2018-11-05 10:04:06 +0100

    update json

--- gyuto-web/src/components/data.js
+++ gyuto-web/src/components/data.js
@@ -1,379 +1,136 @@
-export const login = [
-  {
-    user: "",
-    password: ""
-  }
-];
+export const admin = {
+  user: "",
+  email: "",
+  password: ""
+};
 
-export const videos = [
-  {
-    id: "",
-    title: [
-      {
+export const introduction = {
+  url: "",
+  legend: {
+    fr: "",
+    en: "",
+  }
+};
+
+export const videos = {
+  id: "",
+  title: {
+    fr: "",
+    en: ""
+  },
+  category: "",
+  quote: {
+    fr: "",
+    en: ""
+  },
+  legend: {
+    fr: "",
+    en: ""
+  },
+  url: "",
+  gif: ""
+};
+
+export const events = {
+  id: "",
+  title: {
+    fr: "",
+    en: ""
+  },
+  date: {
+    start: "",
+    end: "",
+  },
+  hour: {
+    start: "",
+    end: "",
+  },
+  adress: {
+    number: "",
+    street: "",
+    zipcode: "",
+    country: "",
+    city: ""
+  },
+  description: {
+    fr: "",
+    en: ""
+  },
+  image: ""
+},
+
+
+export const images = {
+  id: "",
+  title: {
+    fr: "",
+    en: ""
+  },
+  galery: {
+    image: {
+      url: "",
+      title: {
+        fr: "",
+        en: ""
+      },
+      legend: {
         fr: "",
         en: ""
       }
-    ],
-    type: "",
-    quote: [
-      {
-        fr: "",
-        en: ""
-      }
-    ],
-    legend: [
-      {
-        fr: "",
-        en: ""
-      }
-    ],
+    }
+  },
+  cover: ""
+};
+
+export const musique = {
+  video: {
     url: "",
-    gif: "",
-    duration: ""
-  }
-];
-
-export const events = [
-  {
-    id: "1",
-    title: [
-      {
-        fr: "blabla",
-        en: "bloblo"
-      }
-    ],
-    date: "10-02-2017",
-    hour: "11:00",
-    place: "Brussels",
-    description: [
-      {
-        fr: "blablibloubloblurp",
-        en: "blablibloubloblurp"
-      }
-    ],
-    url: "https://picsum.photos/200/400?random"
+    legend: {
+      fr: "",
+      en: ""
+    }
   },
-  {
-    id: "2",
-    title: [
-      {
-        fr: "Prière",
-        en: "Prayer"
-      }
-    ],
-    date: "10-02-2017",
-    hour: "11:00",
-    place: "London",
-    description: [
-      {
-        fr: "La prière du matin par un moine",
-        en: "Prayer of a monk in the morning"
-      }
-    ],
-    url: "https://picsum.photos/200/400?random"
+  text: {
+    title: {
+      fr: "",
+      en: ""
+    },
+    text: {
+      fr: "",
+      en: ""
+    }
   },
-  {
-    id: "3",
-    title: [
-      {
-        fr: "Jeux",
-        en: "Game"
-      }
-    ],
-    date: "10-02-2017",
-    hour: "11:00",
-    place: "India",
-    description: [
-      {
-        fr: "Jeux avec des enfants",
-        en: "Games with children"
-      }
-    ],
-    url: "https://picsum.photos/200/400?random"
-  },
-  {
-    id: "3",
-    title: [
-      {
-        fr: "Jeux",
-        en: "Game"
-      }
-    ],
-    date: "10-02-2017",
-    hour: "11:00",
-    place: "India",
-    description: [
-      {
-        fr: "Jeux avec des enfants",
-        en: "Games with children"
-      }
-    ],
-    url: "https://picsum.photos/200/400?random"
-  }
-
-];
-
-export const images = [
-  {
+  album: {
     id: "",
-    title: [
-      {
+    title: "",
+    url_order: "",
+    musics_list: {
+      music: {
+        id: "",
+        url: "",
+        title: ""
+      }
+    }
+  }
+};
+
+export const info = {
+  id: "",
+  title: {
+    fr: "",
+    en: ""
+  },
+  text: {
+    fr: "",
+    en: ""
+  },
+  galery: {
+    image: {
+      url: "",
+      title: {
         fr: "",
         en: ""
       }
-    ],
-    date: "",
-    cover: "",
-    images: [
-      {
-        description: [
-          {
-            fr: "",
-            en: ""
-          }
-        ],
-        files: [""]
-      }
-    ]
+    }
   }
-];
-
-export const musique = [
-  {
-    video_url: "",
-    title: [
-      {
-        fr: "",
-        en: ""
-      }
-    ],
-    description: [
-      {
-        images: [""],
-        text: [
-          {
-            fr: "",
-            en: ""
-          }
-        ]
-      }
-    ],
-    musiques: [
-      {
-        id: "",
-        title: "",
-        file: ""
-      }
-    ],
-    albums: [
-      {
-        id: "",
-        image: "",
-        url_order: ""
-      }
-    ]
-  }
-];
-
-export const info = [
-  {
-    synopsis: [
-      {
-        images: [""],
-        text: [
-          {
-            fr: "",
-            en: ""
-          }
-        ]
-      }
-    ],
-    projet_genesis: [
-      {
-        images: [""],
-        text: [
-          {
-            fr: "",
-            en: ""
-          }
-        ]
-      }
-    ],
-    note_of_intent: [
-      {
-        images: [""],
-        text: [
-          {
-            fr: "",
-            en: ""
-          }
-        ]
-      }
-    ],
-    note_of_production: [
-      {
-        images: [""],
-        text: [
-          {
-            fr: "",
-            en: ""
-          }
-        ],
-        proximity: [
-          {
-            images: [""],
-            text: [
-              {
-                fr: "",
-                en: ""
-              }
-            ]
-          }
-        ],
-        buddhism: [
-          {
-            images: [""],
-            text: [
-              {
-                fr: "",
-                en: ""
-              }
-            ],
-            dharma: [
-              {
-                images: [""],
-                text: [
-                  {
-                    fr: "",
-                    en: ""
-                  }
-                ]
-              }
-            ],
-            sangha: [
-              {
-                images: [""],
-                text: [
-                  {
-                    fr: "",
-                    en: ""
-                  }
-                ]
-              }
-            ],
-            impermanence: [
-              {
-                images: [""],
-                text: [
-                  {
-                    fr: "",
-                    en: ""
-                  }
-                ]
-              }
-            ],
-            dalai_lama: [
-              {
-                images: [""],
-                text: [
-                  {
-                    fr: "",
-                    en: ""
-                  }
-                ]
-              }
-            ]
-          }
-        ],
-        sound_of_gyuto: [
-          {
-            images: [""],
-            text: [
-              {
-                fr: "",
-                en: ""
-              }
-            ]
-          }
-        ],
-        parables: [
-          {
-            images: [""],
-            text: [
-              {
-                fr: "",
-                en: ""
-              }
-            ]
-          }
-        ],
-        girls_notebooks: [
-          {
-            images: [""],
-            text: [
-              {
-                fr: "",
-                en: ""
-              }
-            ]
-          }
-        ],
-        gestures_of_rituals: [
-          {
-            images: [""],
-            text: [
-              {
-                fr: "",
-                en: ""
-              }
-            ]
-          }
-        ],
-        characters: [
-          {
-            images: [""],
-            text: [
-              {
-                fr: "",
-                en: ""
-              }
-            ]
-          }
-        ]
-      }
-    ],
-    place_description: [
-      {
-        images: [""],
-        text: [
-          {
-            fr: "",
-            en: ""
-          }
-        ]
-      }
-    ],
-    cv: [
-      {
-        images: [""],
-        text: [
-          {
-            fr: "",
-            en: ""
-          }
-        ]
-      }
-    ],
-    credits: [
-      {
-        images: [""],
-        text: [
-          {
-            fr: "",
-            en: ""
-          }
-        ]
-      }
-    ]
-  }
-];
+};
